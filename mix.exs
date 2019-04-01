6a26d39b4ccf14377fe9de64dd4452d23bd7ac40
monslow <mikeo@clarityvoice.com>
2019-03-31 22:12:23 -0400

    ...

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule LiveviewDemo.MixProject do
       {:phoenix, github: "phoenixframework/phoenix", branch: "v1.4"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.11"},
-#      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
