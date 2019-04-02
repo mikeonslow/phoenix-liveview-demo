2d7334048a6d7d2332c5c7149b49bf2f1d234fd0
mikeonslow <mikeonslow@gmail.com>
2019-04-01 23:18:39 -0400

    Updated front-end templating to draw portfolio items and categories

--- lib/liveview_demo_web/views/portfolio_view.ex
+++ lib/liveview_demo_web/views/portfolio_view.ex
@@ -1,3 +1,21 @@
 defmodule LiveviewDemoWeb.PortfolioView do
   use LiveviewDemoWeb, :view
+
+  def get_categories(model) do
+    model["portfolio"]["categories"]
+  end
+
+  def get_items(model) do
+    model["portfolio"]["items"]
+  end
+
+  def selected_category_id?(model, categoryId) do
+    IO.puts("Cat matches")
+    IO.inspect({model["selectedCategoryId"], categoryId})
+    model["selectedCategoryId"] == categoryId |> IO.inspect()
+  end
+
+  def get_selected_item_id(model) do
+    model["selectedItemId"]
+  end
 end
