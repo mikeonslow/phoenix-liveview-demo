2f3cbdb9f8620d48589f39e38d9b46c189c8f48b
mikeonslow <mikeonslow@gmail.com>
2019-04-01 23:17:56 -0400

    Added initial model and interactions for selecting a category ID

--- lib/liveview_demo_web/live/portfolio_live.ex
+++ lib/liveview_demo_web/live/portfolio_live.ex
@@ -3,19 +3,48 @@ defmodule LiveviewDemoWeb.PortfolioLive do
   alias LiveviewDemoWeb.PortfolioView
 
   def mount(_session, socket) do
-    {:ok, assign(socket, :val, 5)}
+    {code, portfolio} = LiveviewDemo.Portfolio.Server.get()
+
+    initialModel =
+      {200,
+       %{
+         "errorMessage" => "",
+         "portfolio" => portfolio,
+         "selectedCategoryId" => map_default_selected_category_id(portfolio["categories"]),
+         "selectedItemId" => 0
+       }}
+
+    initialModel
+    |> map_assigns(socket)
   end
 
-  def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+  def handle_event("select_category", selectedCategoryId, socket) do
+    {:noreply, assign(socket, update_selected_category_id(socket, selectedCategoryId))}
   end
 
-  def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
+  def handle_event("select_item", data, socket) do
+    {:noreply, update(socket, :selectedItemId, data)}
   end
 
   def render(assigns) do
     PortfolioView.render("index.html", assigns)
   end
 
+  def map_assigns({200, model}, socket) do
+    newSocket = socket |> assign(:model, model)
+    {:ok, newSocket}
+  end
+
+  defp update_selected_category_id(
+         %{assigns: %{model: model} = assigns} = socket,
+         selectedCategoryId
+       ) do
+    %{
+      assigns
+      | model: %{model | "selectedCategoryId" => selectedCategoryId |> String.to_integer()}
+    }
+  end
+
+  defp map_default_selected_category_id([]), do: 0
+  defp map_default_selected_category_id([hd | _rest]), do: hd["id"]
 end
